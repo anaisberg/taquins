bedc7490a20eaba9dd787eea4c87af5711250918
anaisberg <48248701+anaisberg@users.noreply.github.com>
2019-03-28 13:49:15 +0100

    Update taquins.java
    Modification de getVoisinVide pour que le programme renvoie un tableau à deux éléments si jamais il y a deux possibilités

--- taquins.java
+++ taquins.java
@@ -25,7 +25,7 @@ public class taquins extends Frame implements ActionListener, ItemListener {
 	 * public enum P { C1, C2, C3, C4, C, R1haut, R1bas, R2g, R2d, vide; }
 	 */
 
-	// P pour Pièces
+	// P pour PiÃ¨ces
 
 	public static P aneRouge1[][] = { { P.R1haut, P.C1, P.C2, P.R1haut }, { P.R1bas, P.C3, P.C4, P.R1bas },
 			{ P.R1haut, P.R2g, P.R2d, P.R1haut }, { P.R1bas, P.vide, P.vide, P.R1bas }, { P.C, P.C, P.C, P.C } };
@@ -55,10 +55,10 @@ public class taquins extends Frame implements ActionListener, ItemListener {
 	public static P aneRouge9[][] = { { P.R2g, P.R2d, P.R2g, P.R2d }, { P.R1haut, P.C1, P.C2, P.R1haut },
 			{ P.R1bas, P.C3, P.C4, P.R1bas }, { P.C, P.R2g, P.R2g, P.C }, { P.C, P.vide, P.vide, P.C } };
 
-	// C1,C2,C3,C4 = carré 2x2 (chacune de ses cases)
+	// C1,C2,C3,C4 = carrÃ© 2x2 (chacune de ses cases)
 	// R1haut, R1bas = rectangle 2x1
 	// R2g, R2d = rectangle 1x2
-	// C = carré 1x1
+	// C = carrÃ© 1x1
 	// null = case vide
 
 	public P[][][] configurations = { aneRouge1, aneRouge2, aneRouge3, aneRouge4, aneRouge5, aneRouge6, aneRouge7,
@@ -68,7 +68,7 @@ public class taquins extends Frame implements ActionListener, ItemListener {
 
 	public taquins() {
 
-		super("Taquins"); // paramètre = titre de la fenêtre
+		super("Taquins"); // paramÃ¨tre = titre de la fenÃªtre
 
 		dessin = new taquinsCanvas();
 		dessin.setSize(250, 250);
@@ -85,7 +85,7 @@ public class taquins extends Frame implements ActionListener, ItemListener {
 		affichageCompteur = new Label("nombre de coups= 0");
 		etat = new Label("Partie en cours");
 
-		// enregistrement de l'application elle-meme comme ecouteur des événements de
+		// enregistrement de l'application elle-meme comme ecouteur des Ã©vÃ©nements de
 		// type Action :
 		retour.addActionListener(this);
 		rejouer.addActionListener(this);
@@ -111,7 +111,7 @@ public class taquins extends Frame implements ActionListener, ItemListener {
 
 	}
 
-	// écouteurs
+	// Ã©couteurs
 
 	public void actionPerformed(ActionEvent evt) {
 
@@ -124,7 +124,7 @@ public class taquins extends Frame implements ActionListener, ItemListener {
 		}
 
 		if (evt.getSource() == retour) {
-			// mettre la configuration précédente
+			// mettre la configuration prÃ©cÃ©dente
 			dessin.repaint();
 		}
 
@@ -142,7 +142,7 @@ public class taquins extends Frame implements ActionListener, ItemListener {
 		}
 	}
 
-	// méthodes
+	// mÃ©thodes
 
 	public P[][] choixConfigAleat() {
 
@@ -157,82 +157,105 @@ public class taquins extends Frame implements ActionListener, ItemListener {
 		// 0 <= i <= 4
 		// 0 <= j <= 3
 		if (conf[3][1].equals(P.C1) && conf[3][2].equals(P.C2) && conf[4][1].equals(P.C3) && conf[4][2].equals(P.C4)) {
-			etat.setText("Vous avez gagné ! bravo !");
+			etat.setText("Vous avez gagnÃ© ! bravo !");
 		}
 	}
 
-	public static String getVoisinVide(int i, int j) {
-		String resultat = "null";
+	public static String[] getVoisinVide(int i, int j) {
+		//String resultat = "null";
 		int hauteur = 5;
 		int largeur = 4;
+		String tab[] = new String[2] ;
+		// on fait un tableau des voisins vides car il peut y en avoir plusieurs
+		int k = 0 ;
+		// nombre de voisins vides
 
 		if (i != 0 && i != hauteur - 1 && j != largeur - 1 && j != 0) {
 			if (conf[i - 1][j].equals(P.vide)) {
-				resultat = "dessus";
+				tab[k] = "dessus";
+				k++;
 			}
 			if (conf[i + 1][j].equals(P.vide)) {
-				resultat = "sous";
-			}
+				tab[k] = "sous";
+				k++;
+				}
 			if (conf[i][j - 1].equals(P.vide)) {
-				resultat = "gauche";
-			}
+				tab[k] = "gauche";
+				k++;
+				}
 			if (conf[i][j + 1].equals(P.vide)) {
-				resultat = "droite";
-			}
+				tab[k] = "droite";
+				k++;
+				}
 		}
 
 		if (i == 0) {
 			if (j == 0) {
 				if (conf[i + 1][j].equals(P.vide)) {
-					resultat = "sous";
-				}
+					tab[k] = "sous";
+					k++;
+					}
 				if (conf[i][j + 1].equals(P.vide)) {
-					resultat = "droite";
-				}
+					tab[k] = "droite";
+					k++;
+					}
 			}
 
 			if (j == largeur - 1) {
 				if (conf[i][j - 1].equals(P.vide)) {
-					resultat = "gauche";
-				}
+					tab[k] = "gauche";
+					k++;
+					}
 				if (conf[i + 1][j].equals(P.vide)) {
-					resultat = "sous";
+					tab[k] = "sous";
+					k++;
+					
 				}
 			}
 
 			if (j != 0 && j != largeur - 1) {
 				if (conf[i + 1][j].equals(P.vide)) {
-					resultat = "sous";
-				}
+					tab[k] = "sous";
+					k++;
+					}
 
 				if (conf[i][j - 1].equals(P.vide)) {
-					resultat = "gauche";
+					tab[k] = "gauche";
+					k++;
+					
 				}
 				if (conf[i][j + 1].equals(P.vide)) {
-					resultat = "droite";
-				}
+					tab[k] = "droite";
+					k++;
+					}
 			}
 		}
 
 		if (j == 0) {
 			if (i == hauteur - 1) {
 				if (conf[i - 1][j].equals(P.vide)) {
-					resultat = "dessus";
-				}
+					tab[k] = "dessus";
+					k++;
+					}
 				if (conf[i][j + 1].equals(P.vide)) {
-					resultat = "droite";
+					tab[k] = "droite";
+					k++;
+					}
 				}
-			}
 
 			if (i != hauteur - 1 && i != 0) {
 				if (conf[i + 1][j].equals(P.vide)) {
-					resultat = "sous";
-				}
+					tab[k] = "sous";
+					k++;
+					}
 				if (conf[i - 1][j].equals(P.vide)) {
-					resultat = "dessus";
+					tab[k] = "dessus";
+					k++;
 				}
 				if (conf[i][j + 1].equals(P.vide)) {
-					resultat = "droite";
+					tab[k] = "droite";
+					k++;
+					
 				}
 			}
 		}
@@ -240,231 +263,56 @@ public class taquins extends Frame implements ActionListener, ItemListener {
 		if (i == hauteur - 1) {
 			if (j == largeur - 1) {
 				if (conf[i - 1][j].equals(P.vide)) {
-					resultat = "dessus";
-				}
+					tab[k] = "dessus";
+					k++;
+					}
 				if (conf[i][j - 1].equals(P.vide)) {
-					resultat = "gauche";
-				}
+					tab[k] = "gauche";
+					k++;
+					}
 			}
 
 			if (j != largeur - 1 && j != 0) {
 				if (conf[i - 1][j].equals(P.vide)) {
-					resultat = "dessus";
-				}
+					tab[k] = "dessus";
+					k++;
+					}
 				if (conf[i][j - 1].equals(P.vide)) {
-					resultat = "gauche";
+					tab[k] = "gauche";
+					k++;
 				}
 				if (conf[i][j + 1].equals(P.vide)) {
-					resultat = "droite";
-				}
+					tab[k] = "droite";
+					k++;
+					}
 			}
 		}
 
 		if (j == largeur - 1) {
 			if (i != hauteur - 1 && i != 0) {
 				if (conf[i - 1][j].equals(P.vide)) {
-					resultat = "dessus";
-				}
+					tab[k] = "dessus";
+					k++;
+					}
 				if (conf[i + 1][j].equals(P.vide)) {
-					resultat = "sous";
+					tab[k] = "sous";
+					k++;
+				
 				}
 				if (conf[i][j - 1].equals(P.vide)) {
-					resultat = "gauche";
-				}
+					tab[k] = "gauche";
+					k++;
+					}
 			}
 		}
 
-		return resultat;
+		return tab;
 	}
-
 	@Override
 	public void itemStateChanged(ItemEvent arg0) {
 		// TODO Auto-generated method stub
 
 	}
-
-	/*
-	 * public static void mouse(MouseEvent evt) {
-	 * 
-	 * System.out.println("on a cliqué");
-	 * 
-	 * int x = evt.getX(); int y = evt.getY(); int w = 250; int h = 300;
-	 * 
-	 * if (x <= w && y <= h) { int i = y % 50; int j = x % 50;
-	 * 
-	 * P[][] grille = conf.clone(); P piece = conf[i][j]; switch (piece) { case C1:
-	 * if (getVoisinVide(i, j) == "dessus" && getVoisinVide(i, j + 1) == "dessus") {
-	 * grille[i][j] = P.C3; grille[i][j + 1] = P.C4; grille[i + 1][j] = P.vide;
-	 * grille[i + 1][j + 1] = P.vide; grille[i - 1][j] = P.C1; grille[i - 1][j + 1]
-	 * = P.C2; compteur++; record.empiler(grille);
-	 * 
-	 * } if (getVoisinVide(i + 1, j) == "sous" && getVoisinVide(i + 1, j + 1) ==
-	 * "sous") { grille[i][j] = P.vide; grille[i][j + 1] = P.vide; grille[i + 1][j]
-	 * = P.C1; grille[i + 1][j + 1] = P.C2; grille[i + 2][j] = P.C3; grille[i + 2][j
-	 * + 1] = P.C4; compteur++; record.empiler(grille); } if (getVoisinVide(i, j) ==
-	 * "gauche" && getVoisinVide(i - 1, j) == "gauche") { grille[i][j] = P.C2;
-	 * grille[i][j + 1] = P.vide; grille[i + 1][j] = P.C4; grille[i + 1][j + 1] =
-	 * P.vide; grille[i][j - 1] = P.C1; grille[i + 1][j - 1] = P.C3; compteur++;
-	 * record.empiler(grille); } if (getVoisinVide(i, j + 1) == "droite" &&
-	 * getVoisinVide(i - 1, j + 1) == "droite") { grille[i][j] = P.vide; grille[i][j
-	 * + 1] = P.C1; grille[i + 1][j] = P.vide; grille[i + 1][j + 1] = P.C3;
-	 * grille[i][j + 2] = P.C2; grille[i + 1][j + 2] = P.C4; compteur++;
-	 * record.empiler(grille); } break;
-	 * 
-	 * case C2: if (getVoisinVide(i, j) == "dessus" && getVoisinVide(i, j - 1) ==
-	 * "dessus") { grille[i][j] = P.C4; grille[i][j - 1] = P.C3; grille[i + 1][j] =
-	 * P.vide; grille[i + 1][j - 1] = P.vide; grille[i - 1][j] = P.C2; grille[i -
-	 * 1][j - 1] = P.C1; compteur++; record.empiler(grille);
-	 * 
-	 * }
-	 * 
-	 * if (getVoisinVide(i + 1, j) == "sous" && getVoisinVide(i + 1, j - 1) ==
-	 * "sous") { grille[i][j] = P.vide; grille[i][j - 1] = P.vide; grille[i + 1][j]
-	 * = P.C2; grille[i + 1][j - 1] = P.C1; grille[i + 2][j] = P.C4; grille[i + 2][j
-	 * - 1] = P.C3; compteur++; record.empiler(grille); } if (getVoisinVide(i, j -
-	 * 1) == "gauche" && getVoisinVide(i - 1, j - 1) == "gauche") { grille[i][j] =
-	 * P.vide; grille[i][j - 1] = P.C2; grille[i + 1][j] = P.vide; grille[i + 1][j -
-	 * 1] = P.C4; grille[i][j - 2] = P.C1; grille[i + 1][j - 2] = P.C3; compteur++;
-	 * record.empiler(grille); } if (getVoisinVide(i, j) == "droite" &&
-	 * getVoisinVide(i + 1, j) == "droite") { grille[i][j] = P.C1; grille[i][j + 1]
-	 * = P.C2; grille[i + 1][j] = P.C3; grille[i + 1][j + 1] = P.C4; grille[i][j -
-	 * 1] = P.vide; grille[i + 1][j - 1] = P.vide; compteur++;
-	 * record.empiler(grille); } break;
-	 * 
-	 * case C3: if (getVoisinVide(i - 1, j) == "dessus" && getVoisinVide(i - 1, j +
-	 * 1) == "dessus") { grille[i][j] = P.vide; grille[i][j + 1] = P.vide; grille[i
-	 * - 1][j] = P.C3; grille[i - 1][j + 1] = P.C4; grille[i - 2][j + 1] = P.C2;
-	 * grille[i - 2][j - 1] = P.C1; compteur++; record.empiler(grille); }
-	 * 
-	 * if (getVoisinVide(i, j) == "sous" && getVoisinVide(i, j + 1) == "sous") {
-	 * grille[i][j] = P.C1; grille[i][j + 1] = P.C2; grille[i + 1][j] = P.C3;
-	 * grille[i + 1][j + 1] = P.C4; grille[i - 1][j] = P.vide; grille[i - 1][j + 1]
-	 * = P.vide; compteur++; record.empiler(grille); } if (getVoisinVide(i, j) ==
-	 * "gauche" && getVoisinVide(i - 1, j) == "gauche") { grille[i][j] = P.C4;
-	 * grille[i][j - 1] = P.C3; grille[i - 1][j] = P.C2; grille[i - 1][j - 1] =
-	 * P.C1; grille[i][j + 1] = P.vide; grille[i - 1][j + 1] = P.vide; compteur++;
-	 * record.empiler(grille); } if (getVoisinVide(i, j + 1) == "droite" &&
-	 * getVoisinVide(i - 1, j + 1) == "droite") { grille[i][j] = P.vide; grille[i][j
-	 * + 1] = P.C3; grille[i - 1][j] = P.vide; grille[i - 1][j + 1] = P.C1;
-	 * grille[i][j + 2] = P.C4; grille[i - 1][j + 2] = P.C2; compteur++;
-	 * record.empiler(grille); } break;
-	 * 
-	 * case C4: if (getVoisinVide(i - 1, j) == "dessus" && getVoisinVide(i - 1, j -
-	 * 1) == "dessus") { grille[i][j] = P.vide; grille[i][j - 1] = P.vide; grille[i
-	 * - 1][j] = P.C4; grille[i - 1][j - 1] = P.C3; grille[i - 2][j] = P.C2;
-	 * grille[i - 2][j - 1] = P.C1; compteur++; record.empiler(grille); }
-	 * 
-	 * if (getVoisinVide(i, j) == "sous" && getVoisinVide(i, j - 1) == "sous") {
-	 * grille[i][j] = P.C2; grille[i][j - 1] = P.C1; grille[i + 1][j] = P.C4;
-	 * grille[i + 1][j - 1] = P.C3; grille[i - 1][j] = P.vide; grille[i - 1][j - 1]
-	 * = P.vide; compteur++; record.empiler(grille); } if (getVoisinVide(i, j - 1)
-	 * == "gauche" && getVoisinVide(i - 1, j - 1) == "gauche") { grille[i][j] =
-	 * P.vide; grille[i][j - 1] = P.C4; grille[i - 1][j] = P.vide; grille[i - 1][j -
-	 * 1] = P.C2; grille[i - 1][j - 2] = P.C1; grille[i][j - 2] = P.C3; compteur++;
-	 * record.empiler(grille); } if (getVoisinVide(i, j) == "droite" &&
-	 * getVoisinVide(i - 1, j) == "droite") { grille[i][j] = P.C3; grille[i][j + 1]
-	 * = P.C4; grille[i - 1][j] = P.C1; grille[i - 1][j + 1] = P.C2; grille[i][j -
-	 * 2] = P.vide; grille[i - 1][j - 2] = P.vide; compteur++;
-	 * record.empiler(grille); } break;
-	 * 
-	 * case R1haut: if (getVoisinVide(i, j) == "dessus") { grille[i][j] = P.R1bas;
-	 * grille[i - 1][j] = P.R1haut; grille[i - 2][j] = P.vide; compteur++;
-	 * record.empiler(grille); }
-	 * 
-	 * if (getVoisinVide(i + 1, j) == "sous") { grille[i][j] = P.vide; grille[i +
-	 * 1][j] = P.R1haut; grille[i + 2][j] = P.R1bas; compteur++;
-	 * record.empiler(grille); } if (getVoisinVide(i, j) == "gauche" &&
-	 * getVoisinVide(i + 1, j) == "gauche") { grille[i][j] = P.vide; grille[i][j -
-	 * 1] = P.R1haut; grille[i - 1][j] = P.vide; grille[i - 1][j - 1] = P.R1bas;
-	 * compteur++; record.empiler(grille); } if (getVoisinVide(i, j) == "droite" &&
-	 * getVoisinVide(i + 1, j) == "droite") { grille[i][j] = P.vide; grille[i][j +
-	 * 1] = P.R1haut; grille[i - 1][j] = P.vide; grille[i - 1][j + 1] = P.R1bas;
-	 * compteur++; record.empiler(grille); } break;
-	 * 
-	 * case R1bas: if (getVoisinVide(i - 1, j) == "dessus") { grille[i][j] = P.vide;
-	 * grille[i - 1][j] = P.R1bas; grille[i - 2][j] = P.R1haut; compteur++;
-	 * record.empiler(grille); }
-	 * 
-	 * if (getVoisinVide(i, j) == "sous") { grille[i][j] = P.R1haut; grille[i +
-	 * 1][j] = P.R1bas; grille[i - 1][j] = P.vide; compteur++;
-	 * record.empiler(grille); } if (getVoisinVide(i, j) == "gauche" &&
-	 * getVoisinVide(i - 1, j) == "gauche") { grille[i][j] = P.vide; grille[i][j -
-	 * 1] = P.R1bas; grille[i + 1][j] = P.vide; grille[i + 1][j - 1] = P.R1haut;
-	 * compteur++; record.empiler(grille); } if (getVoisinVide(i, j) == "droite" &&
-	 * getVoisinVide(i - 1, j) == "droite") { grille[i][j] = P.vide; grille[i][j +
-	 * 1] = P.R1bas; grille[i + 1][j] = P.vide; grille[i + 1][j + 1] = P.R1bas;
-	 * compteur++; record.empiler(grille); } break;
-	 * 
-	 * case R2g: if (getVoisinVide(i, j) == "dessus" && getVoisinVide(i, j + 1) ==
-	 * "dessus") { grille[i][j] = P.vide; grille[i][j + 1] = P.vide; grille[i -
-	 * 1][j] = P.R2g; grille[i - 1][j + 1] = P.R2d; compteur++;
-	 * record.empiler(grille); }
-	 * 
-	 * if (getVoisinVide(i, j) == "sous" && getVoisinVide(i, j + 1) == "sous") {
-	 * grille[i][j] = P.vide; grille[i][j + 1] = P.vide; grille[i + 1][j] = P.R2g;
-	 * grille[i + 1][j + 1] = P.R2d; compteur++; record.empiler(grille); } if
-	 * (getVoisinVide(i, j) == "gauche") { grille[i][j] = P.R2d; grille[i][j - 1] =
-	 * P.R2g; grille[i][j + 1] = P.vide; compteur++; record.empiler(grille); } if
-	 * (getVoisinVide(i, j + 1) == "droite") { grille[i][j] = P.vide; grille[i][j +
-	 * 1] = P.R2g; grille[i][j + 2] = P.R2d; compteur++; record.empiler(grille); }
-	 * break;
-	 * 
-	 * case R2d: if (getVoisinVide(i, j) == "dessus" && getVoisinVide(i, j - 1) ==
-	 * "dessus") { grille[i][j] = P.vide; grille[i][j - 1] = P.vide; grille[i -
-	 * 1][j] = P.R2d; grille[i - 1][j - 1] = P.R2g; compteur++;
-	 * record.empiler(grille); }
-	 * 
-	 * if (getVoisinVide(i, j) == "sous" && getVoisinVide(i, j - 1) == "sous") {
-	 * grille[i][j] = P.vide; grille[i][j - 1] = P.vide; grille[i + 1][j] = P.R2d;
-	 * grille[i + 1][j - 1] = P.R2g; compteur++; record.empiler(grille); } if
-	 * (getVoisinVide(i, j - 1) == "gauche") { grille[i][j] = P.vide; grille[i][j -
-	 * 1] = P.R2d; grille[i][j - 2] = P.R2g; compteur++; record.empiler(grille); }
-	 * if (getVoisinVide(i, j) == "droite") { grille[i][j] = P.R2g; grille[i][j + 1]
-	 * = P.R2d; grille[i][j - 1] = P.vide; compteur++; record.empiler(grille); }
-	 * break;
-	 * 
-	 * case C: if (getVoisinVide(i, j) == "dessus") { grille[i][j] = P.vide;
-	 * grille[i - 1][j] = P.C; compteur++; record.empiler(grille); }
-	 * 
-	 * if (getVoisinVide(i, j) == "sous") { grille[i][j] = P.vide; grille[i + 1][j]
-	 * = P.C; compteur++; record.empiler(grille); } if (getVoisinVide(i, j) ==
-	 * "gauche") { grille[i][j] = P.vide; grille[i][j - 1] = P.C; compteur++;
-	 * record.empiler(grille); } if (getVoisinVide(i, j) == "droite") { grille[i][j]
-	 * = P.vide; grille[i][j + 1] = P.C; compteur++; record.empiler(grille); }
-	 * break; default: }
-	 * 
-	 * conf = grille; affichageCompteur.setText("Nombre de coups : " + compteur); }
-	 * dessin.repaint(); verifier();
-	 * 
-	 * }
-	 */
-
-	/*
-	 * @Override public void mouseEntered(MouseEvent arg0) { // TODO Auto-generated
-	 * method stub }
-	 */
-
-	/*
-	 * @Override public void mouseClicked(MouseEvent arg0) { // TODO Auto-generated
-	 * method stub
-	 * 
-	 * }
-	 * 
-	 * @Override public void mouseExited(MouseEvent arg0) { // TODO Auto-generated
-	 * method stub
-	 * 
-	 * }
-	 * 
-	 * @Override public void mousePressed(MouseEvent arg0) { // TODO Auto-generated
-	 * method stub
-	 * 
-	 * }
-	 * 
-	 * @Override public void mouseReleased(MouseEvent arg0) { // TODO Auto-generated
-	 * method stub
-	 * 
-	 * }
-	 */
 
 	public static void main(String args[]) {
 		taquins appli = new taquins();
